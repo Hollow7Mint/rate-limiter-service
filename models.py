93a8fb6c051ebb2c1c7506e58409ab4cd0bb9cd6
James Holloway <james.holloway94@gmail.com>
2026-04-28 12:04:38 +0000

    relay db connection string time through config

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-04-28 10:48:47 UTC
+# Last sync: 2026-04-28 12:04:38 UTC
