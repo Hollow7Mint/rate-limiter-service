f875548c22639f094e9d4d1a6dee58f8fefdb41b
James Holloway <james.holloway94@gmail.com>
2026-04-29 05:12:09 +0000

    pin mongodb shard balancing time in config

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-04-29 04:20:13 UTC
+# Last sync: 2026-04-29 05:12:09 UTC
