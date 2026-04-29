4be48cfd56b7cd61fdb8817f9a31da770f7cae3e
James Holloway <james.holloway94@gmail.com>
2026-04-29 23:47:45 +0000

    bridge postgres foreign key sync through config

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-04-29 23:01:33 UTC
+# Last sync: 2026-04-29 23:47:45 UTC
