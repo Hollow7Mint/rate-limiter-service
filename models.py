525ecc000c818572f8449b5892a0c606202e933b
James Holloway <james.holloway94@gmail.com>
2026-04-29 18:47:53 +0000

    thread postgres wal sync through config banner

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-04-29 17:39:35 UTC
+# Last sync: 2026-04-29 18:47:53 UTC
