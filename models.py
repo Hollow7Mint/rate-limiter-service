1cd7d85b0d76b7bd207b21bc75a413dc89c4bcca
James Holloway <james.holloway94@gmail.com>
2026-04-30 22:50:00 +0000

    resolve mysql backup sync into metadata file

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-04-30 21:11:55 UTC
+# Last sync: 2026-04-30 22:50:00 UTC
