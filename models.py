3ef55d26100c4aa0a33d4fbfb2434bb2083a6c75
James Holloway <james.holloway94@gmail.com>
2026-05-02 05:59:22 +0000

    queue redis keyspace sync after config update

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-02 04:24:11 UTC
+# Last sync: 2026-05-02 05:59:21 UTC
