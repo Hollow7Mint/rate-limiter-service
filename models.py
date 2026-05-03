16dfdf7a8ecf35a0f28f85471c4a36c86eb89e77
James Holloway <james.holloway94@gmail.com>
2026-05-03 21:59:54 +0000

    post ssh key rotation heartbeat to config

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-03 21:02:02 UTC
+# Last sync: 2026-05-03 21:59:54 UTC
