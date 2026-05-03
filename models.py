546330e4f0a9a949fbbdedf531dc6ebf3264f4c2
James Holloway <james.holloway94@gmail.com>
2026-05-03 09:15:11 +0000

    unify ec2 placement sync with metadata heartbeat

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-03 08:56:35 UTC
+# Last sync: 2026-05-03 09:15:11 UTC
