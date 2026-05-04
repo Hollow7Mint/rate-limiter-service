45819e9c870b9002b19e9941cd2e2bfe31fdad40
James Holloway <james.holloway94@gmail.com>
2026-05-04 08:52:05 +0000

    write mongodb index rebuild time into metadata

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-04 07:37:49 UTC
+# Last sync: 2026-05-04 08:52:05 UTC
