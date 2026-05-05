422030c563a835c083b2cae89382cfda43ad507d
James Holloway <james.holloway94@gmail.com>
2026-05-05 13:20:36 +0000

    audit vpc peering sync record after route update

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-05 12:09:10 UTC
+# Last sync: 2026-05-05 13:20:36 UTC
