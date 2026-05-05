ab7db3e9395e72f9a76ccf5d45a9ae8e52074453
James Holloway <james.holloway94@gmail.com>
2026-05-05 10:26:04 +0000

    reconcile cloudwatch dashboard sync after update

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-05 09:46:26 UTC
+# Last sync: 2026-05-05 10:26:04 UTC
