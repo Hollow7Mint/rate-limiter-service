5b309c2a11ba6967e26fac4a28881233d89dc81c
James Holloway <james.holloway94@gmail.com>
2026-05-05 16:01:43 +0000

    lift db replica sync time into banner record

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-05 14:59:44 UTC
+# Last sync: 2026-05-05 16:01:43 UTC
