f844584a9f36f909e2d863f76bdbc37bbe58f315
James Holloway <james.holloway94@gmail.com>
2026-05-05 02:50:58 +0000

    rotate kms key metadata sync after policy apply

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-05 01:41:33 UTC
+# Last sync: 2026-05-05 02:50:58 UTC
