03024c27d11b8571fec3efd63723762680c5a559
James Holloway <james.holloway94@gmail.com>
2026-05-06 03:06:33 +0000

    carry mongodb oplog sync stamp to config

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-06 01:45:54 UTC
+# Last sync: 2026-05-06 03:06:33 UTC
