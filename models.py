79abdd0290f944d2ef3cf1ab331b749db62e255b
James Holloway <james.holloway94@gmail.com>
2026-05-09 11:35:55 +0000

    submit mongodb backup marker after maintenance

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-09 10:46:32 UTC
+# Last sync: 2026-05-09 11:35:55 UTC
