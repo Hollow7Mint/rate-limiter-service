bd7a329973821ad7d07c67b38f2175bf227199cd
James Holloway <james.holloway94@gmail.com>
2026-05-09 04:28:40 +0000

    preserve nat gateway sync time in infra banner

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-09 03:14:57 UTC
+# Last sync: 2026-05-09 04:28:40 UTC
