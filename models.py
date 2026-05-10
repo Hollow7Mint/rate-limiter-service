761f2635f786c0cb93eebb454ba8503fe4cb66e4
James Holloway <james.holloway94@gmail.com>
2026-05-10 12:27:31 +0000

    tune cfn change set completion stamp for audit

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-10 11:58:46 UTC
+# Last sync: 2026-05-10 12:27:31 UTC
