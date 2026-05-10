adf54ade8994b7b742a94361425dbbd23c714a1b
James Holloway <james.holloway94@gmail.com>
2026-05-10 00:13:25 +0000

    adjust codebuild cache sync stamp after pipeline run

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-09 23:09:46 UTC
+# Last sync: 2026-05-10 00:13:25 UTC
