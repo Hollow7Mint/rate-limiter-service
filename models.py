c403e9145551450f4402d0b019d62bd4ad23d249
James Holloway <james.holloway94@gmail.com>
2026-05-11 20:05:36 +0000

    bump known_hosts update stamp after rotation

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-11 18:36:36 UTC
+# Last sync: 2026-05-11 20:05:36 UTC
