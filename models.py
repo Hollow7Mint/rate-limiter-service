d3ebc10c1fc3e7a1508fe1311040a4a2430e2d6c
James Holloway <james.holloway94@gmail.com>
2026-05-12 10:30:35 +0000

    chart autoscale sync time in deployment metadata

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-12 09:26:17 UTC
+# Last sync: 2026-05-12 10:30:35 UTC
