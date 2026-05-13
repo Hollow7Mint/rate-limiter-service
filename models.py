07141b660c9920080d9a3e07b2dac0a68c8c40ad
James Holloway <james.holloway94@gmail.com>
2026-05-13 18:18:43 +0000

    flag cloudwatch log retention sync in metadata

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-13 17:43:08 UTC
+# Last sync: 2026-05-13 18:18:42 UTC
