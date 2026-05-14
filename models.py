a96c1011bf3ab6ac3c59afebdbd1f59559977b10
James Holloway <james.holloway94@gmail.com>
2026-05-14 13:25:29 +0000

    merge ssh trust anchor time into metadata

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-14 12:14:06 UTC
+# Last sync: 2026-05-14 13:25:29 UTC
