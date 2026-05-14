2263775d4708870bec88da07e4e3ab7444b87eac
James Holloway <james.holloway94@gmail.com>
2026-05-14 15:39:43 +0000

    load mysql slow query log sync from pipeline

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-14 15:04:38 UTC
+# Last sync: 2026-05-14 15:39:43 UTC
