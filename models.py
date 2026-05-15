27714f5eb650ba0e14e086797eb007c7f18838c5
James Holloway <james.holloway94@gmail.com>
2026-05-15 03:10:35 +0000

    persist connection string refresh in config banner

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-15 02:45:55 UTC
+# Last sync: 2026-05-15 03:10:35 UTC
