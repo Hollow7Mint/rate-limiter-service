a9ea96fdea6c95898202bbdfb7399155c070024e
James Holloway <james.holloway94@gmail.com>
2026-05-16 08:07:00 +0000

    audit db backup completion time in record

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-16 06:50:24 UTC
+# Last sync: 2026-05-16 08:07:00 UTC
