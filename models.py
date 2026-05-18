34bc22e18b85fcfd71791b25e5d92e1457859b5e
James Holloway <james.holloway94@gmail.com>
2026-05-18 04:32:40 +0000

    sync ssm parameter refresh after pipeline pass

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-18 03:24:21 UTC
+# Last sync: 2026-05-18 04:32:40 UTC
