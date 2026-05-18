0fcd0e9104f1fbd20ae8ca76756dab3bdacb0421
James Holloway <james.holloway94@gmail.com>
2026-05-18 01:48:44 +0000

    inspect iam trust policy sync marker for review

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-18 00:09:12 UTC
+# Last sync: 2026-05-18 01:48:44 UTC
