256bb3c62a98a6f4d1989329f74f717bd5d1f528
James Holloway <james.holloway94@gmail.com>
2026-05-19 01:51:10 +0000

    seal inspector finding sync in security banner

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-19 00:42:26 UTC
+# Last sync: 2026-05-19 01:51:10 UTC
