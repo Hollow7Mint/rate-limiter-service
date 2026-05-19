8512d32e16fd531bbcbb64ceac76b376f67adcd2
James Holloway <james.holloway94@gmail.com>
2026-05-19 11:29:12 +0000

    finalize vpc security group sync after rule push

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-19 10:53:48 UTC
+# Last sync: 2026-05-19 11:29:12 UTC
