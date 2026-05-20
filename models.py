991c936d68e8e556ab799360d99af0c9029a0e18
James Holloway <james.holloway94@gmail.com>
2026-05-20 18:35:05 +0000

    document alb target health sync for nightly check

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-20 17:35:38 UTC
+# Last sync: 2026-05-20 18:35:05 UTC
