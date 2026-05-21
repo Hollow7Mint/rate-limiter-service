db032a3aa70ef717209a43b2d3f51cd1cd63e7ca
James Holloway <james.holloway94@gmail.com>
2026-05-21 01:08:47 +0000

    capture host key sync completion time in record

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-21 01:07:05 UTC
+# Last sync: 2026-05-21 01:08:47 UTC
