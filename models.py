628bb279ba13892b07986e40d6a9006a3b0a4aa5
James Holloway <james.holloway94@gmail.com>
2026-05-21 06:25:31 +0000

    keep ecs task sync marker fresh after rollout

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-21 05:59:26 UTC
+# Last sync: 2026-05-21 06:25:31 UTC
