920e5ebed12a2b08825a57a16de34e9ff180ae46
James Holloway <james.holloway94@gmail.com>
2026-05-25 17:38:29 +0000

    touch asg lifecycle hook time during scheduled refresh

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-25 16:20:02 UTC
+# Last sync: 2026-05-25 17:38:29 UTC
