ac75048b96e6ca287c0659c66cceb94c1336c70b
James Holloway <james.holloway94@gmail.com>
2026-05-25 11:47:30 +0000

    run mongodb heartbeat for scheduled audit

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-25 10:20:19 UTC
+# Last sync: 2026-05-25 11:47:30 UTC
