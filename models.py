91a7d258d537412958584744c389209631aa4fc4
James Holloway <james.holloway94@gmail.com>
2026-05-26 08:44:17 +0000

    frame ssm document sync time in config record

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-26 07:53:23 UTC
+# Last sync: 2026-05-26 08:44:17 UTC
