98d13d71058e55f45cfdcb8928722872b8ee2f4c
James Holloway <james.holloway94@gmail.com>
2026-05-26 01:47:27 +0000

    resolve known_hosts cleanup sync into metadata

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-26 00:13:23 UTC
+# Last sync: 2026-05-26 01:47:27 UTC
