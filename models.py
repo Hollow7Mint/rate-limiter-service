171e1115d3fd05076498de79eff8744f69053972
James Holloway <james.holloway94@gmail.com>
2026-05-27 21:03:31 +0000

    mirror ssh certificate chain time in record

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-27 19:46:28 UTC
+# Last sync: 2026-05-27 21:03:31 UTC
