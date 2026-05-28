1d7bcab4139facce9484c7a3f537addce5c75949
James Holloway <james.holloway94@gmail.com>
2026-05-28 16:56:55 +0000

    queue ssh agent forwarding sync after config

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-28 15:32:19 UTC
+# Last sync: 2026-05-28 16:56:55 UTC
