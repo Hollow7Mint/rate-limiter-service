655ca5252987816b25b52f035c799f2efc3a1e69
James Holloway <james.holloway94@gmail.com>
2026-05-29 05:01:10 +0000

    advance ssh key trust sync into config record

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-29 04:06:26 UTC
+# Last sync: 2026-05-29 05:01:10 UTC
