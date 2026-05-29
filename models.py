e58bd50ceb9db497fe5bd91c5ab967c04b7673cb
James Holloway <james.holloway94@gmail.com>
2026-05-29 02:27:34 +0000

    advance cdk app deploy time into config

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-29 02:05:00 UTC
+# Last sync: 2026-05-29 02:27:34 UTC
