4ca27dabed9ade12416b2b920c032913cdd7a682
James Holloway <james.holloway94@gmail.com>
2026-05-30 22:55:07 +0000

    catalog mongodb document count sync after audit

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-30 21:28:56 UTC
+# Last sync: 2026-05-30 22:55:07 UTC
