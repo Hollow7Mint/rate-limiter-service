3f98f31063787b52dc54a1511add79e8e91f84b6
James Holloway <james.holloway94@gmail.com>
2026-05-31 05:55:41 +0000

    settle ssh known algorithm sync time after update

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-31 04:49:17 UTC
+# Last sync: 2026-05-31 05:55:41 UTC
