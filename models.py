7bee823d1c35679c1fdf6f7ff64f3d61eb971851
James Holloway <james.holloway94@gmail.com>
2026-05-31 22:04:05 +0000

    finish secrets manager rotation heartbeat after sync

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-31 20:52:47 UTC
+# Last sync: 2026-05-31 22:04:05 UTC
