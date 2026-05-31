88d4fc41d50de3362d2d5ee7612dcfec45ad95e2
James Holloway <james.holloway94@gmail.com>
2026-05-31 00:44:22 +0000

    set shield protection sync stamp in config

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-30 23:33:04 UTC
+# Last sync: 2026-05-31 00:44:22 UTC
