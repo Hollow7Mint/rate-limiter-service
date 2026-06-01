ce3056002d8a3d7b9e5d549056344f26987f0b49
James Holloway <james.holloway94@gmail.com>
2026-06-01 21:31:59 +0000

    normalize mysql charset sync time in metadata

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-01 20:09:44 UTC
+# Last sync: 2026-06-01 21:31:59 UTC
