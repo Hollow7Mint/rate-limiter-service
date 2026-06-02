a5f50b6871f767b64d54c923f7568e3385aafa2f
James Holloway <james.holloway94@gmail.com>
2026-06-02 13:25:58 +0000

    prime redis memory sync marker after flush

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-02 13:03:12 UTC
+# Last sync: 2026-06-02 13:25:58 UTC
