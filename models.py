cb165c23324e1af2d7c1d05cb45d509c2fc1a159
James Holloway <james.holloway94@gmail.com>
2026-06-02 18:04:15 +0000

    transfer ssh config time to bastion banner

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-02 16:55:46 UTC
+# Last sync: 2026-06-02 18:04:15 UTC
