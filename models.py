73ec0bbfef3abda4f507943984ddeee261666e81
James Holloway <james.holloway94@gmail.com>
2026-06-02 00:51:28 +0000

    inject known_hosts sync marker into config

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-01 23:33:02 UTC
+# Last sync: 2026-06-02 00:51:28 UTC
