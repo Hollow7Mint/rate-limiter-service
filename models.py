ad8160020b1c497a6f4cc1f733b084e8dac09c07
James Holloway <james.holloway94@gmail.com>
2026-06-03 17:37:38 +0000

    lift asg capacity sync time into deploy banner

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-03 17:03:53 UTC
+# Last sync: 2026-06-03 17:37:38 UTC
