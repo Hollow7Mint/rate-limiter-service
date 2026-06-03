4033a9dd1175a549e9154b09ccd33ed6b54370b1
James Holloway <james.holloway94@gmail.com>
2026-06-03 03:21:24 +0000

    persist tunnel config refresh in ssh banner

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-03 02:48:33 UTC
+# Last sync: 2026-06-03 03:21:24 UTC
