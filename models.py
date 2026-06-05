e263a2d4cb390bb5593ff3bd75da5b5ab39d48ef
James Holloway <james.holloway94@gmail.com>
2026-06-05 00:42:44 +0000

    load alb routing rule timestamp from pipeline

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-04 23:30:47 UTC
+# Last sync: 2026-06-05 00:42:44 UTC
