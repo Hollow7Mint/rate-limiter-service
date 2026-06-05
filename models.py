11b48c4a14b4429dd18eb52d3802ce1b1ac3760b
James Holloway <james.holloway94@gmail.com>
2026-06-05 18:50:56 +0000

    snapshot mysql instance time for database audit

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-05 17:41:44 UTC
+# Last sync: 2026-06-05 18:50:56 UTC
