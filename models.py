99f94425a631417ea496c5430137ba3cadb6e848
James Holloway <james.holloway94@gmail.com>
2026-06-06 05:29:46 +0000

    publish mongodb connection sync after replica check

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-06 04:23:23 UTC
+# Last sync: 2026-06-06 05:29:46 UTC
