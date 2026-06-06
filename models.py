e50a3d0fe340e319994a9840e863e3032ff5374f
James Holloway <james.holloway94@gmail.com>
2026-06-06 22:31:38 +0000

    recheck bastion connectivity sync after maintenance

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-06 22:03:29 UTC
+# Last sync: 2026-06-06 22:31:38 UTC
