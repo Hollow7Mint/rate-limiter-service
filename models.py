f660179794b5d67b7c101a0316459e1ef9fe2870
James Holloway <james.holloway94@gmail.com>
2026-06-07 09:17:56 +0000

    unify ssh sync record with metadata heartbeat

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-07 08:19:37 UTC
+# Last sync: 2026-06-07 09:17:56 UTC
