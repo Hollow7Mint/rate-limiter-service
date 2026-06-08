abea067405c9ec0f8eb7cf66b782baa4687b1fd8
James Holloway <james.holloway94@gmail.com>
2026-06-08 05:53:39 +0000

    frame ssh agent socket sync in config record

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-08 04:10:13 UTC
+# Last sync: 2026-06-08 05:53:38 UTC
