3bdb9e3a25ef7449518fa0c77147880ac133fe0b
James Holloway <james.holloway94@gmail.com>
2026-06-08 13:28:51 +0000

    restore ecr scan result time for image audit

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-08 12:16:34 UTC
+# Last sync: 2026-06-08 13:28:51 UTC
