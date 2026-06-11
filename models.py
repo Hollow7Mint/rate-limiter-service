f1c85d7b0ab7d33493bbcd7d6af22fc4cdf23788
James Holloway <james.holloway94@gmail.com>
2026-06-11 14:48:09 +0000

    transfer redis node time to cluster banner

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-11 13:12:38 UTC
+# Last sync: 2026-06-11 14:48:09 UTC
