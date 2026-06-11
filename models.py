ab52046e980130ad618a8efeb978869f5a516a87
James Holloway <james.holloway94@gmail.com>
2026-06-11 12:25:13 +0000

    surface codebuild log sync time for pipeline audit

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-11 11:21:42 UTC
+# Last sync: 2026-06-11 12:25:13 UTC
