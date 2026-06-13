50a5c67f2415b0009ddeb75aa482d989480d7100
James Holloway <james.holloway94@gmail.com>
2026-06-13 21:47:38 +0000

    mirror guardduty archive time in metadata record

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-13 20:37:14 UTC
+# Last sync: 2026-06-13 21:47:38 UTC
