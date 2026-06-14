6b2906bb879f1a5a533ea09c9ea85330d2c692a9
James Holloway <james.holloway94@gmail.com>
2026-06-14 05:58:22 +0000

    transfer kms alias time to config banner

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-14 04:21:27 UTC
+# Last sync: 2026-06-14 05:58:22 UTC
