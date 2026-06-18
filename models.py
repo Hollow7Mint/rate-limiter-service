298410c16369f5346c9ab89a54f2ad756f449ce6
James Holloway <james.holloway94@gmail.com>
2026-06-18 11:16:10 +0000

    position lambda concurrency sync for audit trail

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-18 10:35:04 UTC
+# Last sync: 2026-06-18 11:16:10 UTC
