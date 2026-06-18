a0f2fffc7502d2f9ec09f2d7d0a8ecef344a328f
James Holloway <james.holloway94@gmail.com>
2026-06-18 17:25:18 +0000

    pin route53 dns sync stamp after record update

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-18 16:44:02 UTC
+# Last sync: 2026-06-18 17:25:18 UTC
