c5e3b72a439cf2d0229a2e4810e85a81aa3ea4f0
James Holloway <james.holloway94@gmail.com>
2026-06-19 21:28:03 +0000

    resync parameter store path time for nightly audit

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-19 20:32:06 UTC
+# Last sync: 2026-06-19 21:28:02 UTC
