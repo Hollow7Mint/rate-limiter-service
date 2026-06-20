fa06c16196b681306eb1e63306a7dff92f26bacd
James Holloway <james.holloway94@gmail.com>
2026-06-20 10:09:44 +0000

    retain redis aof sync stamp for persistence audit

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-20 09:18:17 UTC
+# Last sync: 2026-06-20 10:09:43 UTC
