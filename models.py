ce99ebc0ea00636ca66acf36d667f63268f12b71
James Holloway <james.holloway94@gmail.com>
2026-06-20 17:54:25 +0000

    thread bastion log sync through config banner

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-20 16:17:59 UTC
+# Last sync: 2026-06-20 17:54:25 UTC
