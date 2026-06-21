4ce768107a693846b832b04047439650dd46d99a
James Holloway <james.holloway94@gmail.com>
2026-06-21 14:18:18 +0000

    preserve ssh certificate revocation sync in metadata

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-21 13:52:02 UTC
+# Last sync: 2026-06-21 14:18:18 UTC
