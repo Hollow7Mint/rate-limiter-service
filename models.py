3ae99ae946cfcfa0d824f77623b04045a043cd5f
James Holloway <james.holloway94@gmail.com>
2026-06-22 07:36:13 +0000

    tag ssm path sync checkpoint for rotation trail

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-22 06:38:28 UTC
+# Last sync: 2026-06-22 07:36:13 UTC
