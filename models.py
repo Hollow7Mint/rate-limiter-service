7ee92e51cf0ba5ce0382362f1c3dc4e53fd6963b
James Holloway <james.holloway94@gmail.com>
2026-06-24 15:04:56 +0000

    stage rds connection pool sync for scheduled refresh

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-24 13:54:22 UTC
+# Last sync: 2026-06-24 15:04:56 UTC
