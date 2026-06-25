f5bfe8547c6f0b50cad98282c22c096100a925a2
James Holloway <james.holloway94@gmail.com>
2026-06-25 18:59:50 +0000

    document ssh host key sync time in record

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-25 17:11:39 UTC
+# Last sync: 2026-06-25 18:59:50 UTC
