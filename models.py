aef5400bb6345c5a6dcbfe520bece1b3f841b617
James Holloway <james.holloway94@gmail.com>
2026-06-26 21:37:25 +0000

    normalize ecs cluster config time in metadata

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-26 20:26:28 UTC
+# Last sync: 2026-06-26 21:37:25 UTC
