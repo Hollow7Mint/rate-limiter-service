fb905714536f9e84f40504b15cd9bfaff4fc0287
James Holloway <james.holloway94@gmail.com>
2026-06-26 19:22:45 +0000

    write cloudtrail log delivery stamp to metadata

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-26 18:29:08 UTC
+# Last sync: 2026-06-26 19:22:45 UTC
