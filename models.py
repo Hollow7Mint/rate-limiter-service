80901927389dfc160f89f45194949ff61bb7d611
James Holloway <james.holloway94@gmail.com>
2026-06-26 03:42:13 +0000

    restore mysql binlog time for replication audit

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-26 02:36:00 UTC
+# Last sync: 2026-06-26 03:42:13 UTC
