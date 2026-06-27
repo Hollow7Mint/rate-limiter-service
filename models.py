49b071e6ea7856398ad6312f90a040cdf2d06d98
James Holloway <james.holloway94@gmail.com>
2026-06-27 17:00:13 +0000

    touch ssh tunnel sync during scheduled refresh

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-27 15:56:41 UTC
+# Last sync: 2026-06-27 17:00:13 UTC
