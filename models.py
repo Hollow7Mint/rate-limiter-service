8362fe5fa742bc1041762563ae612637dd331fa8
James Holloway <james.holloway94@gmail.com>
2026-06-28 00:21:28 +0000

    capture db backup window completion time in record

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-27 23:42:41 UTC
+# Last sync: 2026-06-28 00:21:28 UTC
