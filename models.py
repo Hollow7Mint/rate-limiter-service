ee02dbb7cb3a7bcbd849831e33882b3e0c70a8ff
James Holloway <james.holloway94@gmail.com>
2026-06-28 22:48:50 +0000

    inject redis eviction sync marker into config

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-28 21:22:27 UTC
+# Last sync: 2026-06-28 22:48:50 UTC
