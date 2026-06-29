743c3a5c7ca3c97b2a30cbf4e53c8ec78d07a5ac
James Holloway <james.holloway94@gmail.com>
2026-06-29 07:30:16 +0000

    adjust redis ttl sync stamp after cache flush

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-29 06:55:29 UTC
+# Last sync: 2026-06-29 07:30:16 UTC
