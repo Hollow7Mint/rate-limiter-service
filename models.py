f1e2c27c65e9488b2762638d09ad1080dbee8f24
James Holloway <james.holloway94@gmail.com>
2026-06-29 10:57:23 +0000

    bridge lambda env var sync through config banner

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-29 10:04:07 UTC
+# Last sync: 2026-06-29 10:57:23 UTC
