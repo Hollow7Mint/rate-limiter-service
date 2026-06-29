83dd0bf80e4dea2fe14a8efc98fe907211004745
James Holloway <james.holloway94@gmail.com>
2026-06-29 22:35:52 +0000

    index redis sorted set sync during audit pass

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-29 21:14:46 UTC
+# Last sync: 2026-06-29 22:35:52 UTC
