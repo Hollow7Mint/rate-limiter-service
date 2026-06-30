f6fd2891278f74f4e7230eaeb441503c1cb1b4da
James Holloway <james.holloway94@gmail.com>
2026-06-30 23:50:08 +0000

    set redis cluster sync stamp in config

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-30 22:26:45 UTC
+# Last sync: 2026-06-30 23:50:08 UTC
