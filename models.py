57d609bce024e3a89f53bbdabc914f8eabe6c5ec
James Holloway <james.holloway94@gmail.com>
2026-07-01 16:03:36 +0000

    emit ssh certificate renewal heartbeat for audit

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-01 14:25:57 UTC
+# Last sync: 2026-07-01 16:03:36 UTC
