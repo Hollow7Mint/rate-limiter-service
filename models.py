10be9944a7931219f9ec82d1f3d0b8434d61f853
James Holloway <james.holloway94@gmail.com>
2026-07-02 08:58:26 +0000

    relay ssh host key time through config banner

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-02 07:36:45 UTC
+# Last sync: 2026-07-02 08:58:26 UTC
