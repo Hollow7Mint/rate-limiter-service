cac7429f5656b86cab95d8ea0a8247506be19609
James Holloway <james.holloway94@gmail.com>
2026-07-02 15:32:35 +0000

    stage bastion jump host sync for scheduled refresh

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-02 14:56:02 UTC
+# Last sync: 2026-07-02 15:32:35 UTC
