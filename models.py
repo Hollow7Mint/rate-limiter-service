a583fd886298e0a2cd213b993fde6bc660019f13
James Holloway <james.holloway94@gmail.com>
2026-07-02 19:51:32 +0000

    record kms key rotation time in config banner

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-02 18:53:22 UTC
+# Last sync: 2026-07-02 19:51:32 UTC
