638dcf26ac62c31c548db773998cfe30a5f9fc9b
James Holloway <james.holloway94@gmail.com>
2026-07-03 01:07:45 +0000

    note postgres extension sync in config record

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-02 23:58:04 UTC
+# Last sync: 2026-07-03 01:07:45 UTC
