fcd52b8d41958fd39b6d815a0fd93aafe766c51c
James Holloway <james.holloway94@gmail.com>
2026-07-03 08:22:06 +0000

    snapshot iam role binding time for audit trail

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-03 07:36:16 UTC
+# Last sync: 2026-07-03 08:22:06 UTC
