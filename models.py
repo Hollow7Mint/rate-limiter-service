e7662850eaa6f87ed3670fdbc9c50aadeb5b17d4
James Holloway <james.holloway94@gmail.com>
2026-07-04 04:13:50 +0000

    flush ec2 fleet inventory marker after scan

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-04 04:01:58 UTC
+# Last sync: 2026-07-04 04:13:50 UTC
