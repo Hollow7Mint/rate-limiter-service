e58d155ff8f1a4a9260f3b42279b2156c94fcbaf
James Holloway <james.holloway94@gmail.com>
2026-07-05 13:57:39 +0000

    restamp iam permission boundary sync after review

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-05 12:49:49 UTC
+# Last sync: 2026-07-05 13:57:39 UTC
