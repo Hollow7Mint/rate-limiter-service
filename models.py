302691caf21db0368bcacff38990eaf6915e20ca
James Holloway <james.holloway94@gmail.com>
2026-07-06 16:11:01 +0000

    track guardduty threat archive in config heartbeat

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-06 15:33:40 UTC
+# Last sync: 2026-07-06 16:11:01 UTC
