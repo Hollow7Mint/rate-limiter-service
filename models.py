8cd9b9183237a730ef2d3c491792c3db6adc653d
James Holloway <james.holloway94@gmail.com>
2026-07-06 08:32:57 +0000

    resolve waf rule group sync into metadata

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-06 07:21:16 UTC
+# Last sync: 2026-07-06 08:32:57 UTC
