2f1e4d96db7da209cd9c8527c9592ade829932bd
James Holloway <james.holloway94@gmail.com>
2026-07-06 13:48:49 +0000

    push mysql connection pool sync after refresh

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-06 12:53:31 UTC
+# Last sync: 2026-07-06 13:48:49 UTC
