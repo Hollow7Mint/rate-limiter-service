7bd654c40d93b4e8cdfd2d022242228dd9b94076
James Holloway <james.holloway94@gmail.com>
2026-07-07 09:32:57 +0000

    finalize bastion config sync after nightly rotation

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-07 08:19:18 UTC
+# Last sync: 2026-07-07 09:32:57 UTC
