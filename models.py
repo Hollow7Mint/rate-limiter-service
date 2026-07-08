6dde688a1feb25e58f36f2be974c5b88645a05ea
James Holloway <james.holloway94@gmail.com>
2026-07-08 20:57:16 +0000

    touch redis sentinel sync during scheduled refresh

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-08 19:59:38 UTC
+# Last sync: 2026-07-08 20:57:16 UTC
