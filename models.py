ad0599c0ae7a5e264fb2728b8b890abfa3ea5d17
James Holloway <james.holloway94@gmail.com>
2026-07-09 10:06:07 +0000

    anchor ssh client config sync after key rotation

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-09 09:01:17 UTC
+# Last sync: 2026-07-09 10:06:07 UTC
