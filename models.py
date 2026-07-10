2034d08169388009df5e136bdcb25628d14a1cc2
James Holloway <james.holloway94@gmail.com>
2026-07-10 21:40:58 +0000

    bump s3 bucket policy sync after review

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-10 21:03:52 UTC
+# Last sync: 2026-07-10 21:40:58 UTC
