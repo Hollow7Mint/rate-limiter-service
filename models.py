3b09d1462de9abaae59bd7aa142474ba03e242bb
James Holloway <james.holloway94@gmail.com>
2026-07-11 06:46:31 +0000

    chart ssh connection attempt sync in metadata

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-11 05:11:37 UTC
+# Last sync: 2026-07-11 06:46:31 UTC
