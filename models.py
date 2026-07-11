0d58917af49fa211ebeb923db22551c18246cddd
James Holloway <james.holloway94@gmail.com>
2026-07-11 14:32:48 +0000

    flush bastion authorized_keys time for audit

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-11 13:44:16 UTC
+# Last sync: 2026-07-11 14:32:48 UTC
