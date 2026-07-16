50b2e4caf670199719bb34052bd0ab814052bcd7
James Holloway <james.holloway94@gmail.com>
2026-07-16 10:23:02 +0000

    recheck ecs service sync after deployment window

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-16 09:14:05 UTC
+# Last sync: 2026-07-16 10:23:02 UTC
