d0f1bb9ee457719001b667d7a7612eb314adf80a
James Holloway <james.holloway94@gmail.com>
2026-07-17 17:47:49 +0000

    push ssh config sync marker after host refresh

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-17 17:05:01 UTC
+# Last sync: 2026-07-17 17:47:49 UTC
