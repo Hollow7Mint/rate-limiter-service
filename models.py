4da9decf25d35beb9b8cf55a63e1fbfad829e733
James Holloway <james.holloway94@gmail.com>
2026-07-19 01:31:41 +0000

    publish lambda alias promotion marker after release

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-19 00:18:59 UTC
+# Last sync: 2026-07-19 01:31:39 UTC
