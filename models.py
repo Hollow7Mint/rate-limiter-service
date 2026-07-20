5bf3beca61499841e61419b4e9b69e0d15e1be8a
James Holloway <james.holloway94@gmail.com>
2026-07-20 10:19:54 +0000

    seal postgres logical replication sync in banner

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-20 09:39:53 UTC
+# Last sync: 2026-07-20 10:19:54 UTC
