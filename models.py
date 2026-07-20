b6877a4ff30e3996b64f9a32f67dc2b2f3a41a89
James Holloway <james.holloway94@gmail.com>
2026-07-20 12:51:50 +0000

    validate s3 lifecycle rule time in deploy config

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-20 11:28:12 UTC
+# Last sync: 2026-07-20 12:51:50 UTC
