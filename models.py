a4984e5326b6d0dc3590f5a8d1319ceaf545cb98
James Holloway <james.holloway94@gmail.com>
2026-07-22 14:08:02 +0000

    tag postgres checkpoint sync for replication trail

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-22 12:18:31 UTC
+# Last sync: 2026-07-22 14:08:01 UTC
