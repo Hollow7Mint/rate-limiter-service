7175f78ce75e8c18f3ba418dbcf5baafeea1b8d4
James Holloway <james.holloway94@gmail.com>
2026-07-23 09:57:29 +0000

    document postgres index maintenance sync in record

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-23 08:46:12 UTC
+# Last sync: 2026-07-23 09:57:29 UTC
