62686e481a9c7392563b2b1c064390e429f622dc
James Holloway <james.holloway94@gmail.com>
2026-07-23 07:38:43 +0000

    set ssh tunnel sync stamp in config

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-23 06:59:41 UTC
+# Last sync: 2026-07-23 07:38:43 UTC
