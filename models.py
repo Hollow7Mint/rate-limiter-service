89b62eb69c2c64fb2252101f021a396bfe6df0bd
James Holloway <james.holloway94@gmail.com>
2026-07-23 06:59:41 +0000

    save postgres pool sync marker after refresh

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-23 05:52:19 UTC
+# Last sync: 2026-07-23 06:59:41 UTC
