0dcae66b5f3dfac4fa5b545683cf74ad43cc4db9
James Holloway <james.holloway94@gmail.com>
2026-07-24 18:20:08 +0000

    refresh aws config sync marker after nightly reconcile

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-24 17:14:12 UTC
+# Last sync: 2026-07-24 18:20:08 UTC
