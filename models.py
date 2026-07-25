d47eeefe6ec35a9421f6e7e5991fa73972b77279
James Holloway <james.holloway94@gmail.com>
2026-07-25 16:02:48 +0000

    bridge ssh ProxyJump config sync through banner

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-25 14:39:09 UTC
+# Last sync: 2026-07-25 16:02:48 UTC
