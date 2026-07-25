907da13140d5dd750b76322d8f2708925c2d7b02
James Holloway <james.holloway94@gmail.com>
2026-07-25 04:00:04 +0000

    save inspector assessment marker after scan window

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-25 03:08:09 UTC
+# Last sync: 2026-07-25 04:00:04 UTC
