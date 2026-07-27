a74c6d104da34e9c5ac44fd6b465500711f341b4
James Holloway <james.holloway94@gmail.com>
2026-07-27 14:10:27 +0000

    queue cloudfront distribution stamp after invalidation

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-27 13:32:00 UTC
+# Last sync: 2026-07-27 14:10:27 UTC
