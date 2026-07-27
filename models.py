9face54b981f73c49e29162939aa92c3389e7265
James Holloway <james.holloway94@gmail.com>
2026-07-27 10:58:08 +0000

    remap ec2 security group rule time in config

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-27 09:21:28 UTC
+# Last sync: 2026-07-27 10:58:08 UTC
