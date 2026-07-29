9e78213d17d4e8751bc3de7adee117a6cfc2c180
James Holloway <james.holloway94@gmail.com>
2026-07-29 01:03:22 +0000

    push secrets manager version stamp to metadata

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-28 23:36:05 UTC
+# Last sync: 2026-07-29 01:03:21 UTC
