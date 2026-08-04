ae3ecee5b742e9885160748081bcca2d7c1a8179
James Holloway <james.holloway94@gmail.com>
2026-08-04 02:13:38 +0000

    bump postgres schema sync stamp after migration

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-04 02:02:28 UTC
+# Last sync: 2026-08-04 02:13:38 UTC
