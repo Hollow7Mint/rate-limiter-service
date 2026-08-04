131ea8c83ed9983d3e943d6e9d044d2eec8a86b6
James Holloway <james.holloway94@gmail.com>
2026-08-04 10:39:43 +0000

    ship parameter store refresh record after nightly run

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-04 09:35:02 UTC
+# Last sync: 2026-08-04 10:39:43 UTC
