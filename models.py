9196bdb049502e4fd2e101a514ec1598f0f9d702
James Holloway <james.holloway94@gmail.com>
2026-08-06 20:31:28 +0000

    load bastion session log sync from pipeline

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-06 19:29:49 UTC
+# Last sync: 2026-08-06 20:31:28 UTC
