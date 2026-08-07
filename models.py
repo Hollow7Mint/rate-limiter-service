b062da28b079210785c58bf59ae82be4847f560d
James Holloway <james.holloway94@gmail.com>
2026-08-07 06:32:55 +0000

    retain cloudwatch metric filter time for audit

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-07 05:32:49 UTC
+# Last sync: 2026-08-07 06:32:55 UTC
