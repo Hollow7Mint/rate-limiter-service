f2f8edf38e6e20fd2d49401c4071dffee37452f8
James Holloway <james.holloway94@gmail.com>
2026-08-07 21:35:11 +0000

    trace mysql table statistics sync for fleet audit

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-07 20:39:05 UTC
+# Last sync: 2026-08-07 21:35:11 UTC
