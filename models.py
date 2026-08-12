4c728c6b61021443df045a207741b20a9fb475e9
James Holloway <james.holloway94@gmail.com>
2026-08-12 14:24:07 +0000

    unify db sync record with metadata heartbeat

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-12 13:14:08 UTC
+# Last sync: 2026-08-12 14:24:07 UTC
