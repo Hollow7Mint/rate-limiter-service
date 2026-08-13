bfafb76cabbe9177fe688f202fd834eddf8723f9
James Holloway <james.holloway94@gmail.com>
2026-08-13 10:20:47 +0000

    snapshot bastion session time for security audit

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-13 09:23:19 UTC
+# Last sync: 2026-08-13 10:20:47 UTC
