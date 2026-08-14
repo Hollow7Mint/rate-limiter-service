e479ce2062697e678927854a765b77045a6a6515
James Holloway <james.holloway94@gmail.com>
2026-08-14 18:34:51 +0000

    anchor mysql parameter sync after maintenance window

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-14 17:52:16 UTC
+# Last sync: 2026-08-14 18:34:51 UTC
