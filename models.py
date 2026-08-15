9210314a477f76f6d4ab56be915182bc6a797dab
James Holloway <james.holloway94@gmail.com>
2026-08-15 12:27:36 +0000

    rebuild nat gateway sync record after route change

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-15 11:57:05 UTC
+# Last sync: 2026-08-15 12:27:36 UTC
