8077a1f461a9fa880ce46b9168a413480a6147b5
James Holloway <james.holloway94@gmail.com>
2026-08-16 20:05:10 +0000

    stage postgres tablespace sync for scheduled refresh

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-16 18:29:51 UTC
+# Last sync: 2026-08-16 20:05:10 UTC
