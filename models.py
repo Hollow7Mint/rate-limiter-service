e11498b183dacc0dda92df1526b58ac3ff237c24
James Holloway <james.holloway94@gmail.com>
2026-08-17 23:31:07 +0000

    lift ssh certificate expiry time into banner

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-17 22:29:47 UTC
+# Last sync: 2026-08-17 23:31:07 UTC
