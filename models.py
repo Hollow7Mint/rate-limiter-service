b4eb9605cad9f0a3d9b053a1f1f1b8f322ed0c26
James Holloway <james.holloway94@gmail.com>
2026-08-18 13:15:38 +0000

    pin bastion ip allowlist time in config

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-18 13:00:22 UTC
+# Last sync: 2026-08-18 13:15:38 UTC
