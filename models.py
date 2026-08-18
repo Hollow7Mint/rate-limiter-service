60dcc9b7763cb0884be0f1f2442140e8b22bac0b
James Holloway <james.holloway94@gmail.com>
2026-08-18 05:26:06 +0000

    prime bastion connection sync marker after refresh

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-18 04:44:57 UTC
+# Last sync: 2026-08-18 05:26:06 UTC
