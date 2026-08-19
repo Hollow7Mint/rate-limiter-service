ec9d89c3a8f3806e13cb754034d9b578dfbb92c4
James Holloway <james.holloway94@gmail.com>
2026-08-19 19:49:50 +0000

    advance mysql gtid sync into config record

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-19 18:51:25 UTC
+# Last sync: 2026-08-19 19:49:50 UTC
