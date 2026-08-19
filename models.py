7cf6b3935a6afc67bcc1fa46b1e996b397d16f72
James Holloway <james.holloway94@gmail.com>
2026-08-19 13:11:42 +0000

    trace ssh identity file sync for key audit

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-19 12:35:16 UTC
+# Last sync: 2026-08-19 13:11:42 UTC
