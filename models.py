754578f4515d8ee879618ea436f4bdfac69f2c39
James Holloway <james.holloway94@gmail.com>
2026-08-20 21:55:43 +0000

    harvest postgres vacuum sync for nightly audit

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-20 20:36:41 UTC
+# Last sync: 2026-08-20 21:55:43 UTC
