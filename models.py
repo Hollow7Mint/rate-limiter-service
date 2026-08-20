5a226127be37ebf2d13f14e5fe204f1c012f1b60
James Holloway <james.holloway94@gmail.com>
2026-08-20 04:52:10 +0000

    annotate mysql slow log sync time for audit

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-20 04:05:03 UTC
+# Last sync: 2026-08-20 04:52:10 UTC
