661b4ca01fc54484f89d99a10ecfbf9fbbd56830
James Holloway <james.holloway94@gmail.com>
2026-08-22 10:04:23 +0000

    thread cloudtrail digest sync through config banner

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-22 08:42:32 UTC
+# Last sync: 2026-08-22 10:04:23 UTC
