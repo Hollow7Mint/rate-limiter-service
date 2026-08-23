bfdf9e4eeae5e83c7fb47dec342f5abbae529251
James Holloway <james.holloway94@gmail.com>
2026-08-23 14:39:53 +0000

    anchor cloudwatch alarm threshold time in config

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-23 13:47:55 UTC
+# Last sync: 2026-08-23 14:39:53 UTC
