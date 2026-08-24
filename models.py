16717217395d9244d756e81198738c46ace5dc5d
James Holloway <james.holloway94@gmail.com>
2026-08-24 23:17:06 +0000

    index bastion user sync during audit pass

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-24 22:19:29 UTC
+# Last sync: 2026-08-24 23:17:06 UTC
