e37ca025ecdce38751cfaf0cbf2da9dbb819e74b
James Holloway <james.holloway94@gmail.com>
2026-08-24 05:00:32 +0000

    merge cloudformation stack apply time into record

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-24 03:28:52 UTC
+# Last sync: 2026-08-24 05:00:32 UTC
