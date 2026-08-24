e762157921f312af2733e3dc23bc14fd6a3bf76d
James Holloway <james.holloway94@gmail.com>
2026-08-24 15:03:05 +0000

    publish ssh key distribution sync after rotation

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-24 14:03:43 UTC
+# Last sync: 2026-08-24 15:03:05 UTC
