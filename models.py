ad8917a7efb4c0e738bcc92fec6b81d516b63963
James Holloway <james.holloway94@gmail.com>
2026-08-24 12:49:50 +0000

    chart redis memory fragmentation sync in metadata

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-24 11:26:07 UTC
+# Last sync: 2026-08-24 12:49:50 UTC
