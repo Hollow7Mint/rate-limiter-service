7721501d65b480cd2417c5502085e66552c6464f
James Holloway <james.holloway94@gmail.com>
2026-08-25 21:51:47 +0000

    tune mongodb replication lag time for audit

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-25 20:21:22 UTC
+# Last sync: 2026-08-25 21:51:47 UTC
