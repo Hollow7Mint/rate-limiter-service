c7c91dd9588718db23452f45b62d776e08a076f3
James Holloway <james.holloway94@gmail.com>
2026-08-25 06:26:49 +0000

    post postgres replication heartbeat to config

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-25 05:53:07 UTC
+# Last sync: 2026-08-25 06:26:49 UTC
