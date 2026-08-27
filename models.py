81238c1f951a62bed3b5805127239291f2b7a3d9
James Holloway <james.holloway94@gmail.com>
2026-08-27 10:10:00 +0000

    inspect postgres lock wait sync for performance

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-27 08:45:59 UTC
+# Last sync: 2026-08-27 10:10:00 UTC
