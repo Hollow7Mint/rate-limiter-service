3b33df4a2fe3b13488a7cfb8687bea24469c57c6
James Holloway <james.holloway94@gmail.com>
2026-08-27 04:40:59 +0000

    tag known_hosts sync checkpoint for key trail

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-27 03:46:54 UTC
+# Last sync: 2026-08-27 04:40:59 UTC
