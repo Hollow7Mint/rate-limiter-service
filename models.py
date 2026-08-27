adee15c4664d5db3e6a1afc3b66d21800b17bcf7
James Holloway <james.holloway94@gmail.com>
2026-08-27 22:15:42 +0000

    harvest ssh fingerprint sync for nightly audit

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-27 21:19:35 UTC
+# Last sync: 2026-08-27 22:15:41 UTC
