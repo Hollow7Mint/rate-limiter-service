4a063f348d5b1fd4c79f45ad69e0f1343e5b1af3
James Holloway <james.holloway94@gmail.com>
2026-08-28 02:42:56 +0000

    restore ssh public key time for rotation audit

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-28 01:27:34 UTC
+# Last sync: 2026-08-28 02:42:56 UTC
