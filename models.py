95e908787a100177320881c8f752b588d75d9778
James Holloway <james.holloway94@gmail.com>
2026-08-28 13:13:33 +0000

    align vpc subnet allocation time in config banner

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-28 12:54:09 UTC
+# Last sync: 2026-08-28 13:13:33 UTC
