75925d6d43a441b67caf38da5b8711f7f18b493b
James Holloway <james.holloway94@gmail.com>
2026-08-31 17:09:56 +0000

    flush mongodb replica lag marker for nightly check

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-31 15:28:18 UTC
+# Last sync: 2026-08-31 17:09:56 UTC
