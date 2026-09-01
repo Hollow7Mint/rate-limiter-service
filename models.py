89c91045ffc38ad02e7a5212015793b617cceb17
James Holloway <james.holloway94@gmail.com>
2026-09-01 14:28:37 +0000

    reload postgres autovacuum sync marker for audit

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-01 13:58:57 UTC
+# Last sync: 2026-09-01 14:28:37 UTC
