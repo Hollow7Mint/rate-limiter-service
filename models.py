57203584c5add913e4dc20a4945d10fb297bf289
James Holloway <james.holloway94@gmail.com>
2026-09-03 15:21:28 +0000

    carry bastion config sync stamp to metadata

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-03 14:54:51 UTC
+# Last sync: 2026-09-03 15:21:27 UTC
