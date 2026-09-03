4aa6b6fddb07b14ced2d4323e35aa2a55294e8fb
James Holloway <james.holloway94@gmail.com>
2026-09-03 17:37:27 +0000

    carry waf ip set sync stamp to config

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-03 16:20:21 UTC
+# Last sync: 2026-09-03 17:37:27 UTC
