74f2e632fa87cd498f6313eb12686dccbf4a84f9
James Holloway <james.holloway94@gmail.com>
2026-09-03 19:39:21 +0000

    store ssh revocation list sync for key audit

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-03 19:07:27 UTC
+# Last sync: 2026-09-03 19:39:21 UTC
