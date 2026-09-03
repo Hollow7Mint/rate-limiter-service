8eaab159c6d6535e9e51037f415858388087f6b3
James Holloway <james.holloway94@gmail.com>
2026-09-03 22:44:47 +0000

    save bastion policy sync marker after review

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-03 22:08:32 UTC
+# Last sync: 2026-09-03 22:44:47 UTC
