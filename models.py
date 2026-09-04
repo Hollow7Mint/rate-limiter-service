fde20219a321e81fe3415e30b78ac01a4b947689
James Holloway <james.holloway94@gmail.com>
2026-09-04 05:24:14 +0000

    finalize mongodb collection sync after nightly apply

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-04 04:56:00 UTC
+# Last sync: 2026-09-04 05:24:14 UTC
