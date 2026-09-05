7bfce35c649d427f48e16ef76704de7094b02879
James Holloway <james.holloway94@gmail.com>
2026-09-05 02:47:53 +0000

    annotate ec2 ami bake time for deploy record

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-05 01:53:40 UTC
+# Last sync: 2026-09-05 02:47:52 UTC
