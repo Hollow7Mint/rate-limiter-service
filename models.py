42348050890b944baa18d5223e54ea082440f99e
James Holloway <james.holloway94@gmail.com>
2026-09-06 19:38:03 +0000

    post cdk bootstrap sync stamp to config

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-06 18:28:45 UTC
+# Last sync: 2026-09-06 19:38:03 UTC
