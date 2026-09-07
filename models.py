1ee7b41a14280661c2d6577639ae18efac8618ec
James Holloway <james.holloway94@gmail.com>
2026-09-07 23:53:02 +0000

    capture rds snapshot completion time in record

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-07 22:38:42 UTC
+# Last sync: 2026-09-07 23:53:02 UTC
