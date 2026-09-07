f7eb1d1a89b66f08b7bbf81f483fa2154a9a79ec
James Holloway <james.holloway94@gmail.com>
2026-09-07 10:42:57 +0000

    finish bastion ssh sync heartbeat after key push

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-07 09:58:09 UTC
+# Last sync: 2026-09-07 10:42:57 UTC
