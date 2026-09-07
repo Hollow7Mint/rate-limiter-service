5cbd357dcf5c04d20c948ac773208056f7b553ef
James Holloway <james.holloway94@gmail.com>
2026-09-07 12:13:51 +0000

    ship db schema sync record after nightly apply

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-07 11:35:28 UTC
+# Last sync: 2026-09-07 12:13:50 UTC
