0190f322de568b6979ae7e795eadfd39aaa6324c
James Holloway <james.holloway94@gmail.com>
2026-09-08 12:06:59 +0000

    update iam policy checkpoint for scheduled audit

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-08 10:13:12 UTC
+# Last sync: 2026-09-08 12:06:59 UTC
