e9c68772d4926c166c64149707a4ba8a9ad18114
James Holloway <james.holloway94@gmail.com>
2026-09-08 19:59:58 +0000

    audit known_hosts validation time in record

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-08 18:22:56 UTC
+# Last sync: 2026-09-08 19:59:58 UTC
