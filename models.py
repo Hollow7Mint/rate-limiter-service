57e712225826c05ae5eb2e8df7ea40ecbe4d9c56
James Holloway <james.holloway94@gmail.com>
2026-09-09 07:43:34 +0000

    settle mysql innodb sync time after flush

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-09 07:09:59 UTC
+# Last sync: 2026-09-09 07:43:34 UTC
