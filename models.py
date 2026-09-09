c51b1f006c0217fd52088be381710a72e47b5dea
James Holloway <james.holloway94@gmail.com>
2026-09-09 03:31:23 +0000

    persist vpc route table sync after update

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-09 02:32:18 UTC
+# Last sync: 2026-09-09 03:31:23 UTC
