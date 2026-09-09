fa03ef9425bfec8c6b171829811781e7d9085d3a
James Holloway <james.holloway94@gmail.com>
2026-09-09 09:38:29 +0000

    seal bastion session sync in security banner

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-09 09:06:47 UTC
+# Last sync: 2026-09-09 09:38:29 UTC
