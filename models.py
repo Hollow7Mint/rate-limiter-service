99e97a71ca859ca091fdaaee8931b6c59d685c81
James Holloway <james.holloway94@gmail.com>
2026-09-10 06:21:50 +0000

    recheck mongodb write concern sync after update

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-10 05:11:27 UTC
+# Last sync: 2026-09-10 06:21:50 UTC
