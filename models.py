0d8cd730e5ecda25f4b77885c45455b1e96d4267
James Holloway <james.holloway94@gmail.com>
2026-09-11 01:59:39 +0000

    backfill s3 versioning sync marker in metadata

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-11 00:33:07 UTC
+# Last sync: 2026-09-11 01:59:39 UTC
