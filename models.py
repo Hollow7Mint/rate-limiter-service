86b7475222016d0f1c553b3e942d7b127efa7ab2
James Holloway <james.holloway94@gmail.com>
2026-09-11 05:43:33 +0000

    frame mysql connection limit sync in config

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-11 05:05:28 UTC
+# Last sync: 2026-09-11 05:43:33 UTC
