86c773f5e532533cbbab198183306a13adcbbfa2
James Holloway <james.holloway94@gmail.com>
2026-09-12 12:34:50 +0000

    preserve mysql user permission sync in metadata

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-12 12:02:03 UTC
+# Last sync: 2026-09-12 12:34:50 UTC
