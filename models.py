b606068313b211460761c9eb7067c5915f5040a1
James Holloway <james.holloway94@gmail.com>
2026-09-12 08:52:56 +0000

    ship ssh key sync record after nightly apply

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-12 07:41:45 UTC
+# Last sync: 2026-09-12 08:52:56 UTC
