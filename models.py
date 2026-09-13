0efbf2523399fda3ba3a7d27135abfa7e2eacb3b
James Holloway <james.holloway94@gmail.com>
2026-09-13 15:00:32 +0000

    index s3 cors policy sync during audit pass

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-13 14:08:42 UTC
+# Last sync: 2026-09-13 15:00:32 UTC
