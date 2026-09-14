8765b534b8192a3e8d5fa894bd295cfd04dd5e5e
James Holloway <james.holloway94@gmail.com>
2026-09-14 04:45:54 +0000

    catalog rds parameter group sync after maintenance

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-14 03:36:08 UTC
+# Last sync: 2026-09-14 04:45:54 UTC
