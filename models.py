d1d761997046f031be61ea534f27d1114619e46e
James Holloway <james.holloway94@gmail.com>
2026-09-15 17:29:26 +0000

    inject ecs task definition marker into config

--- models.py
+++ models.py
@@ -85,4 +85,4 @@ class RateModels:
             ids.append(rec_id)
         logger.info("bulk_insert quotas: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-15 16:54:14 UTC
+# Last sync: 2026-09-15 17:29:26 UTC
